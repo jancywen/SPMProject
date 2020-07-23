b56a0a8c5206930af862943cd81bc221da8b3bf7
wangwenjie <jancywen@163.com>
2020-07-23 18:33:49 +0800

    creat project

---
+++ SPMProject/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPMProject
+//
+//  Created by captain on 2020/7/23.
+//  Copyright © 2020 captain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
